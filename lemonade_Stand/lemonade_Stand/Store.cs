d1ed963c07f254bfe4ecc6ad2c151bb00c341445
Charles Clark <ctcb57@gmail.com>
2019-08-07 14:48:11 -0500

    Corrected method to prevent user from attempting to buy too much product while also preventing them from entering a string as an answer

--- lemonade_Stand/lemonade_Stand/Store.cs
+++ lemonade_Stand/lemonade_Stand/Store.cs
@@ -29,139 +29,81 @@ namespace lemonade_Stand
         public int SellLemons(Player player, Inventory inventory)
         {
             Console.WriteLine("How many lemons would you like to purchase?");
-            int lemonsPurchased = int.Parse(Console.ReadLine());
-            if(player.cashOnHand < (lemonPrice * lemonsPurchased))
+            int response;
+
+            while(!int.TryParse(Console.ReadLine(), out response) || player.cashOnHand < (lemonPrice * response))
             {
-                Console.WriteLine("You do not have enough cash to make that purchase");
-                Console.WriteLine("Please select a lower amount of this item");
-                Console.ReadLine();
-                SellLemons(player, inventory);
-                return 0;
+                Console.WriteLine("You entered an invalid amount");
+                Console.WriteLine("How many lemons would you like to purchase?");
             }
-            else
-            {
-                player.cashOnHand -= (lemonPrice * lemonsPurchased);
-                inventory.lemonCount += (1 * lemonsPurchased);
-                int moneySpentOnLemons = lemonPrice * lemonsPurchased;
-                return moneySpentOnLemons;
-            } 
+            player.cashOnHand -= (lemonPrice * response);
+            inventory.lemonCount += (1 * response);
+            int moneySpentOnLemons = lemonPrice * response;
+            return moneySpentOnLemons;
         }
 
         public int SellIce(Player player, Inventory inventory)
         {
-            Console.WriteLine("How much ice would you like to purchase?");
-            int icePurchased = int.Parse(Console.ReadLine());
-            if(player.cashOnHand < (icePrice * icePurchased))
+            Console.WriteLine("How many ice packs would you like to purchase?");
+            int response;
+
+            while (!int.TryParse(Console.ReadLine(), out response) || player.cashOnHand < (icePrice * response))
             {
-                Console.WriteLine("You do not have enough cash to make that purchase");
-                Console.WriteLine("Please select a lower amount of this item");
-                Console.ReadLine();
-                SellIce(player, inventory);
-                return 0;
+                Console.WriteLine("You entered an invalid amount");
+                Console.WriteLine("How many ice packs would you like to purchase?");
             }
-            else
-            {
-                player.cashOnHand -= (icePrice * icePurchased);
-                inventory.iceCount += (1 * icePurchased);
-                int moneySpentOnIce = icePrice * icePurchased;
-                return moneySpentOnIce;
-            }
-            
+            player.cashOnHand -= (icePrice * response);
+            inventory.iceCount += (1 * response);
+            int moneySpentOnIce = icePrice * response;
+            return moneySpentOnIce;
         }
 
         public int SellSugar(Player player, Inventory inventory)
         {
-            Console.WriteLine("How many sugar packs would you like to purchase?");
-            int sugarPurchased = int.Parse(Console.ReadLine());
-            if(player.cashOnHand < (sugarPrice * sugarPurchased))
+            Console.WriteLine("How many cups of sugar would you like to purchase?");
+            int response;
+
+            while (!int.TryParse(Console.ReadLine(), out response) || player.cashOnHand < (sugarPrice * response))
             {
-                Console.WriteLine("You do not have enough cash to make that purchase");
-                Console.WriteLine("Please select a lower amount of this item");
-                Console.ReadLine();
-                SellSugar(player, inventory);
-                return 0;
+                Console.WriteLine("You entered an invalid amount");
+                Console.WriteLine("How many cups of sugar would you like to purchase?");
             }
-            else
-            {
-                player.cashOnHand -= (sugarPrice * sugarPurchased);
-                inventory.sugarCount += (1 * sugarPurchased);
-                int moneySpentOnSugar = sugarPrice * sugarPurchased;
-                return moneySpentOnSugar;
-            }
+            player.cashOnHand -= (sugarPrice * response);
+            inventory.sugarCount += (1 * response);
+            int moneySpentOnSugar = sugarPrice * response;
+            return moneySpentOnSugar;
         }
 
-
-        public int SellCup(Player player, Inventory inventory)
+        public int SellCups(Player player, Inventory inventory)
         {
             Console.WriteLine("How many cups would you like to purchase?");
-            int cupsPurchased = int.Parse(Console.ReadLine());
-            if(player.cashOnHand < cupPrice)
-            {
-                Console.WriteLine("You do not have enough cash to make that purchase");
-                Console.WriteLine("Please select a lower amount of this item");
-                Console.ReadLine();
-                SellCup(player, inventory);
-                return 0;
-            }
-            else
-            {
-                player.cashOnHand -= (cupPrice * cupsPurchased);
-                inventory.cupCount += (1 * cupsPurchased);
-                int moneySpentOnCups = cupPrice * cupsPurchased;
-                return moneySpentOnCups;
-            }
+            int response;
 
-            
+            while (!int.TryParse(Console.ReadLine(), out response) || player.cashOnHand < (cupPrice * response))
+            {
+                Console.WriteLine("You entered an invalid amount");
+                Console.WriteLine("How many cups would you like to purchase?");
+            }
+            player.cashOnHand -= (cupPrice * response);
+            inventory.cupCount += (1 * response);
+            int moneySpentOnCups = cupPrice * response;
+            return moneySpentOnCups;
         }
+
 
         public void PurchaseItems(Inventory inventory, Player player)
         {
             inventory.DisplayItemCount();
-            if (player.cashOnHand >= lemonPrice)
-            {
-                int moneySpentOnLemons = SellLemons(player, inventory);
-                player.DisplayCashOnHand();
-                totalPurchase += moneySpentOnLemons;
-                if(player.cashOnHand >= cupPrice)
-                {
-                    int moneySpentOnSugar = SellSugar(player, inventory);
-                    player.DisplayCashOnHand();
-                    totalPurchase += moneySpentOnSugar;
-
-                    if (player.cashOnHand >= icePrice)
-                    {
-                        int moneySpentOnIce = SellIce(player, inventory);
-                        player.DisplayCashOnHand();
-                        totalPurchase += moneySpentOnIce;
-                        if(player.cashOnHand >= sugarPrice)
-                        {
-                            int moneySpentOnCups = SellCup(player, inventory);
-                            player.DisplayCashOnHand();
-                            totalPurchase += moneySpentOnCups;
-                        }
-                        else
-                        {
-                            Console.WriteLine(player.name + " doesn't have enough money to purchase any other items.");
-                            Console.ReadLine();
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine(player.name + " doesn't have enough money to purchase any other items.");
-                        Console.ReadLine();
-                    }
-                }
-                else
-                {
-                    Console.WriteLine(player.name + " doesn't have enough money to purchase any other items.");
-                    Console.ReadLine();
-                }
-            }
-            else
-            {
-                Console.WriteLine(player.name + " doesn't have enough money to purchase any other items.");
-                Console.ReadLine();
-            }
+            player.DisplayCashOnHand();
+            int moneySpentOnLemons = SellLemons(player, inventory);
+            player.DisplayCashOnHand();
+            int moneySpentOnSugar = SellSugar(player, inventory);
+            player.DisplayCashOnHand();
+            int moneySpentOnIce = SellIce(player, inventory);
+            player.DisplayCashOnHand();
+            int moneySpentOnCups = SellCups(player, inventory);
+            player.DisplayCashOnHand();
+            totalPurchase = moneySpentOnLemons + moneySpentOnSugar + moneySpentOnIce + moneySpentOnCups;
             inventory.DisplayItemCount();
             Console.WriteLine(player.name + " spent $" + totalPurchase + " on supplies today.");
             Console.ReadLine();
